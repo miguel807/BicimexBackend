8d5389b1c4ad161b910a96717a370a2d488f83ae
Migue <miguelfernandezperez69@gmail.com>
2025-06-15 09:35:34 -0600

    fix/ urls

--- bicimexBackend/urls.py
+++ bicimexBackend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('comments.urls')),  # Mejor prefijo 'api/'
+    path('api/', include('comments.urls')), 
 ]
